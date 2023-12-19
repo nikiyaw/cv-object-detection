8c59b95d0ba80a3413a994d5896408ab931b1079
Niki Yaw <129553207+nikiyaw@users.noreply.github.com>
2023-12-19 21:29:02 +0800

    Add files via upload

---
+++ yolo.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+import cv2
+
+# Load a pretrained YOLOv8n model
+model = YOLO('yolov8n.pt')
+
+# load video
+video_path = './test.mp4'
+cap = cv2.VideoCapture(video_path)
+
+ret = True
+# read frames
+while ret: 
+    ret, frame = cap.read()
+
+    # detect & track objects
+    results = model.track(frame, persist=True)
+
+    # plot results 
+    frame_ = results[0].plot()
+
+    # visualize
+    cv2.imshow('frame', frame_)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break 
